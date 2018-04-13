3d6a7932bc197929610c820e164cc7c137ba1892
what-00 <what-00@qq.com>
2018-04-13 12:04:20 +0800

    css version 1.10

--- resources/views/zh/detail.blade.php
+++ resources/views/zh/detail.blade.php
@@ -21,8 +21,8 @@
     <meta property="twitter:card" content="summary" />
     <meta property="twitter:site" content="@inwecryptocom" />
     <meta property="twitter:title" content="{{ $title }}" />
-    <meta property="twitter:description" content="{{ $desc ?? $title }}" />
-    <meta property="twitter:url" content="{{ $img ?? \URL::secureAsset('/assets/images/eicon1.png')}}" />
+    <meta property="twitter:description" content="{{ $desc ?: $title }}" />
+    <meta property="twitter:url" content="{{ $img ?: url('/assets/images/eicon1.png')}}" />
     <!-- End -->
 
     <!--  OpenGraph -->
@@ -31,7 +31,7 @@
     <meta property="og:title" content="{{ $title }}" />
     <meta property="og:type" content="article" />
     <meta property="og:url" content="{{ Request::path() }}" />
-    <meta property="og:description" content="{{ $desc ?? $title }}" />
+    <meta property="og:description" content="{{ $desc ?: $title }}" />
     <!-- End -->
 
     <link rel="shortcut icon" href="/favicon.ico">
