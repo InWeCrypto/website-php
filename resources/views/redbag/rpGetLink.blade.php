62c3ea52c1ce8c0a7fdff331dec6dfb41288d70d
liangjunljlj <453021745@qq.com>
2018-05-10 17:01:48 +0800

    添加版本号

--- resources/views/redbag/rpGetLink.blade.php
+++ resources/views/redbag/rpGetLink.blade.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8">
-  <title>领取红包的文本领取红包的文本</title>
+  <title>红包</title>
   <meta name="renderer" content="webkit" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=0">
@@ -18,15 +18,15 @@
   <meta name="x5-page-mode" content="app">
   <meta name="apple-mobile-web-app-status-bar-style" content="default" />
 
-  <meta name="keywords" content="标题标题标题" />
-  <meta name="description" content="简介简介简介简介简介简介简介简介简介" />
+  <meta name="keywords" content="红包" />
+  <meta name="description" content="红包" />
 
   <!--  twitter -->
   <meta property="twitter:card" content="summary" />
   <meta property="twitter:site" content="@inwecryptocom" />
-  <meta property="twitter:title" content="标题标题标题" />
-  <meta property="twitter:description" content="简介简介简介简介简介简介简介简介简介" />
-  <meta property="twitter:url" content="文章图片路径文章图片路径文章图片路径" />
+  <meta property="twitter:title" content="红包" />
+  <meta property="twitter:description" content="红包" />
+  <meta property="twitter:url" content="" />
   <!-- End -->
 
   <!--  OpenGraph -->
@@ -34,14 +34,14 @@
   <meta property="og:site_name" content="inwecrypto">
   <meta property="og:title" content="" />
   <meta property="og:type" content="article" />
-  <meta property="og:url" content="文章网址文章网址文章网址文章网址" />
-  <meta property="og:image" content="文章图片路径文章图片路径文章图片路径" />
-  <meta property="og:description" content="简介简介简介简介简介简介简介简介简介" />
+  <meta property="og:url" content="" />
+  <meta property="og:image" content="" />
+  <meta property="og:description" content="红包" />
   <!-- End -->
 
   <link rel="shortcut icon" href="/favicon.ico">
-  <link rel="stylesheet" href="/assets/css/base.css" />
-  <link rel="stylesheet" href="/assets/css/rpGetLink.css">
+  <link rel="stylesheet" href="/assets/css/base.css?{{ $jss_version }}" />
+  <link rel="stylesheet" href="/assets/css/rpGetLink.css?{{ $jss_version }}">
   <script type="text/javascript" src="/assets/js/jquery.min.js"></script>
   <script type="text/javascript" src="/assets/js/util.js"></script>
   <script type="text/javascript" src="/assets/js/md5.js"></script>
