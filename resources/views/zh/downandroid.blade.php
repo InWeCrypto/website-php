384a0c5a11e1d1b563cc6cc4fb8e423a8626e587
liangjunljlj <453021745@qq.com>
2018-05-16 14:28:18 +0800

    新增安卓下载介绍页

---
+++ resources/views/zh/downandroid.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>InWeCrypto Android下载</title>
+  <meta name="renderer" content="webkit" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="mobile-web-app-capable" content="yes">
+  <meta name="apple-touch-fullscreen" content="yes">
+  <meta name="screen-orientation" content="portrait">
+  <meta name="full-screen" content="yes">
+  <meta name="browsermode" content="application">
+  <meta name="x5-orientation" content="portrait">
+  <meta name="x5-fullscreen" content="true">
+  <meta name="x5-page-mode" content="app">
+  <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+  <meta name="keywords" content="InWeCrypto Android下载"/>
+  <meta name="description" content="安装InWeCrypto的Android版APP"/>
+  <!--  twitter -->
+	<meta property="twitter:card" content="summary" />
+  <meta property="twitter:site" content="@inwecryptocom" />
+  <meta property="twitter:title" content="InWeCrypto Android下载" />
+  <meta property="twitter:description" content="安装InWeCrypto的Android版APP" />
+  <meta property="twitter:url" content="/assets/images/eicon1.png" />
+	<!-- End -->
+
+	<!--  OpenGraph -->
+  <meta property="fb:app_id" content="inwecryptocom"/>
+  <meta property="og:site_name" content="inwecrypto">
+  <meta property="og:title" content="InWeCrypto Android下载"/>
+  <meta property="og:type" content="article"/>
+  <meta property="og:url" content="{{ Request::path() }}"/>
+	<meta property="og:image" content="/assets/images/eicon1.png"/>
+  <meta property="og:description" content="安装InWeCrypto的Android版APP "/>
+  <!-- End -->
+
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="stylesheet" href="/assets/css/base.css?{{ $jss_version }}" />
+  <link rel="stylesheet" href="/assets/css/downios.css?{{ $jss_version }}" />
+  <script type="text/javascript" src="/assets/js/jquery.min.js?{{ $jss_version }}"></script>
+  <script type="text/javascript" src="/assets/js/util.js?{{ $jss_version }}"></script>
+</head>
+
+<body style="padding-left: 54px; padding-right: 54px;" class=" pace-done">
+    <div class="container m-container e-hugeDownIosBox" id="e-hugeDownIosBox">
+        <div class="imgCover1">
+            <img src="/assets/images/dyou_yingying.png" alt="" />
+        </div>
+        <div class="headerBox">
+            <script src="/template/top.js?{{ $jss_version }}"></script>
+            <p class="mess1">如何安装InWeCrypto的Android版APP</p>
+            <p class="mess2">
+                <img src="/assets/images/fiosqrcode.jpg" alt="" />
+            </p>
+            <p class="mess3">扫描二维码下载链接</p>
+            <button class="downloadIosBtn mView" onclick="location.href='zh/download_app'">下载</button>
+        </div>
+        <script src="/template/footer.js?{{ $jss_version }}"></script>
+    </div>
+</body>
+
+</html>
