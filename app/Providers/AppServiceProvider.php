5e89245e3920bfe5fdd94fc12bbfbbf7fb3e38ca
liangjunljlj <453021745@qq.com>
2018-05-18 03:29:12 +0800

    1.47

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
         });
 
         view()->share('lang', \App::getLocale());
-        view()->share('jss_version', 'version=1.46');
+        view()->share('jss_version', 'version=1.47');
     }
 
     /**
